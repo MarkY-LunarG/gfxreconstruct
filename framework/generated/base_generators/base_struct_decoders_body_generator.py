8ce37fd3f35db113a8ead35c074afa41bd4888e1
Mark Young <marky@lunarg.com>
2024-10-23 15:10:51 -0400

    codgen: Remove stale dx12 paths
    Dx12 codegen now uses its own scripts.  So remove any dx12 specific
    items in the Vulkan or Base code.

--- framework/generated/base_generators/base_struct_decoders_body_generator.py
+++ framework/generated/base_generators/base_struct_decoders_body_generator.py
@@ -119,10 +119,6 @@ class BaseStructDecodersBodyGenerator():
 
                 if is_static_array:
                     array_dimension = ''
-                    # dx12 treats 2d array as 1d array. EX: [8][2] -> [16], so dx12's 2d array needs *. 
-                    # But vk keeps 2d array.
-                    if self.is_dx12_class() and value.array_dimension and value.array_dimension > 0:
-                        array_dimension = '*'
                     # The pointer decoder will write directly to the struct member's memory.
                     body += '    wrapper->{name}{}SetExternalMemory({}value->{name}, {arraylen});\n'.format(
                         access_op,
